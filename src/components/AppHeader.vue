a8b2523a0f033639f0223af73f1e81a5dd1e1bd0
marcobusacca <marcobusacca01@gmail.com>
2023-06-22 17:12:58 +0200

    [FEAT] Appheader vue circle

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -12,13 +12,13 @@ export default {
             <div class="row">
                 <div class="header-col">
                     <!-- Blue Big Button -->
-                    <div class="btn-big-blue"></div>
+                    <div class="circle big-circle circle-blue"></div>
                     <!-- Red Button -->
-                    <div class="circle btn-red"></div>
+                    <div class="circle small-circle circle-red"></div>
                     <!-- Yellow Button -->
-                    <div class="circle btn-yellow"></div>
+                    <div class="circle small-circle circle-yellow"></div>
                     <!-- Green Button -->
-                    <div class="circle btn-green"></div>
+                    <div class="circle small-circle circle-green"></div>
                 </div>
             </div>
         </div>
@@ -37,31 +37,37 @@ export default {
         margin: 0 15px;
     }
 
-    .btn-big-blue {
+    .circle {
+        border-radius: 50%;
+        border-style: solid;
+        border-color: #fff;
+    }
+
+    .small-circle {
+        width: 50px;
+        height: 50px;
+        border-width: 3px;
+    }
+
+    .big-circle {
         width: 100px;
         height: 100px;
-        border: 6px solid #fff;
-        border-radius: 50%;
+        border-width: 6px;
+    }
+
+    .circle-blue {
         background: linear-gradient(120deg, rgba(255, 255, 255, 1) 0%, rgba(73, 73, 255, 1) 50%);
     }
 
-    .circle {
-        width: 50px;
-        height: 50px;
-        background-color: #4949FF;
-        border: 3px solid #fff;
-        border-radius: 50%;
-    }
-
-    .btn-red {
+    .circle-red {
         background: linear-gradient(120deg, rgba(255, 255, 255, 1) 0%, #FF3E3E 50%);
     }
 
-    .btn-yellow {
+    .circle-yellow {
         background: linear-gradient(120deg, rgba(255, 255, 255, 1) 0%, #FFFF47 50%);
     }
 
-    .btn-green {
+    .circle-green {
         background: linear-gradient(120deg, rgba(255, 255, 255, 1) 0%, #3D9E3D 50%);
     }
 }
