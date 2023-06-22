6c5551070985b32b9274f3bd880e177bccdda4d6
marcobusacca <marcobusacca01@gmail.com>
2023-06-22 14:23:21 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<!-- JAVASCRIPT & VUE.JS -->
+<script>
+// IMPORTO AXIOS
+import axios from 'axios';
+
+export default {
+
+}
+</script>
+
+<!-- TEMPLATE HTML -->
+<template lang="">
+</template>
+
+<!-- STYLE SCSS -->
+<style lang="scss"></style>
