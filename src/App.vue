784e207b9d4895e816c42c08eb3df05f1d3c5b79
marcobusacca <marcobusacca01@gmail.com>
2023-06-22 16:50:14 +0200

    [FEAT] Chiamata API Pokemon Array Oggetti

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,9 @@ import AppHeader from './components/AppHeader.vue';
 // IMPORTO APP_MAIN
 import AppMain from './components/AppMain.vue';
 
+// IMPORTO STORE.JS
+import { store } from './store.js';
+
 export default {
     // OGGETTO COMPONENTS
     components: {
@@ -17,7 +20,20 @@ export default {
 
         // DICHIARO APP_MAIN
         AppMain
-    }
+    },
+    data() {
+        return {
+            // DICHIARO STORE.JS
+            store,
+        }
+    },
+    created() {
+        // CHIAMATA API POKEMONS
+        axios.get('https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons').then((result) => {
+            // INSERISCO L'ARRAY DI OGGETTI DENTRO STORE.JS
+            store.pokemonList = result.data.docs;
+        })
+    },
 }
 </script>
 
--- src/store.js
+++ src/store.js
@@ -3,4 +3,5 @@ import { reactive } from "vue";
 
 // ESPORTO LA CONSTANTE STORE CONTENENTE TUTTI GLI ELEMENTI ACCESSIBILI AD OGNI COMPONENTE VUE
 export const store = reactive({
+    pokemonList: [],
 });
