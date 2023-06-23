9e8be31a9ca669170336c11025d2121a92725886
marcobusacca <marcobusacca01@gmail.com>
2023-06-23 16:02:35 +0200

    [FIX]

--- src/App.vue
+++ src/App.vue
@@ -28,19 +28,19 @@ export default {
         }
     },
     created() {
+        // CHIAMATA API POKEMONS TYPE
+        axios.get(store.typeApiUrl).then((result) => {
+            // INSERISCO L'ARRAY DI OGGETTI DENTRO STORE.JS
+            store.pokemonTypeList = result.data;
+
+            // INSERISCO DENTRO POKEMON_TYPE_LIST 'ALL' COME PRIMO ELEMENTO
+            store.pokemonTypeList.unshift('All');
+        });
+
         // CHIAMATA API POKEMONS
         axios.get(store.apiUrl).then((result) => {
             // INSERISCO L'ARRAY DI OGGETTI DENTRO STORE.JS
             store.pokemonList = result.data.docs;
-
-            // CHIAMATA API POKEMONS TYPE
-            axios.get(store.typeApiUrl).then((result) => {
-                // INSERISCO L'ARRAY DI OGGETTI DENTRO STORE.JS
-                store.pokemonTypeList = result.data;
-
-                // INSERISCO DENTRO POKEMON_TYPE_LIST 'ALL' COME PRIMO ELEMENTO
-                store.pokemonTypeList.unshift('All');
-            });
 
             // APPENA LA CHIAMATA API è STATA COMPLETATA, SETTO LA VARIABILE LOADING SU FALSE
             store.loading = false;
