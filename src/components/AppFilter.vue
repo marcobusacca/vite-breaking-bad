b0f9be0c0b0cb52681b01dde066b32897cab3138
marcobusacca <marcobusacca01@gmail.com>
2023-06-26 09:46:48 +0200

    Bonus Completato

--- src/components/AppFilter.vue
+++ src/components/AppFilter.vue
@@ -25,7 +25,7 @@ export default {
         <!-- Type Select -->
         <select name="type-select" id="type-select" class="form-select" v-model="store.typeSelected" @change="$emit('filter')">
             <option value="All" selected>All</option>
-            <option v-for="(type, index) in store.pokemonTypeList" :value="type" v-text="type"></option>
+            <option v-for="(type, index) in store.pokemonTypeList" :key="index" :value="type" v-text="type"></option>
         </select>
     </div>
 </template>
